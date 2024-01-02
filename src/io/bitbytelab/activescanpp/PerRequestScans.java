b254fa3cf61e6ac3b346a4725b6993d79e9621f8
Dat Nguyen <dat.nguyen@bitbytelab.io>
2024-01-02 15:52:31 +0700

    Fix scan checks and add new attack triggers

--- src/io/bitbytelab/activescanpp/PerRequestScans.java
+++ src/io/bitbytelab/activescanpp/PerRequestScans.java
@@ -5,15 +5,16 @@ import java.util.ArrayList;
 import burp.*;
 
 public class PerRequestScans implements IScannerCheck {
-    private List<String> IScannerCheck;
+    private List<IScannerCheck> scanChecks;
     private IExtensionHelpers helpers;
     private IBurpExtenderCallbacks callbacks;
 
     public PerRequestScans(IBurpExtenderCallbacks callbacks) {
         this.callbacks = callbacks;
         this.helpers = callbacks.getHelpers();
-        // TODO Auto-generated constructor stub
-        throw new UnsupportedOperationException("Unimplemented constructor 'PerRequestScans'");
+
+        // TODO: Add your own scan checks here
+        return;
     }
 
     @Override
@@ -24,15 +25,25 @@ public class PerRequestScans implements IScannerCheck {
     @Override
     public List<IScanIssue> doActiveScan(IHttpRequestResponse baseRequestResponse,
             IScannerInsertionPoint insertionPoint) {
-
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doActiveScan'");
+        if (!shouldTriggerPerRequestAttacks(baseRequestResponse, insertionPoint)) {
+            return new ArrayList<>();
+        }
+        List<IScanIssue> issues = new ArrayList<>();
+        for (IScannerCheck scanCheck : scanChecks) {
+            try {
+                // issues.addAll(scanCheck.check(baseRequestResponse));
+            } catch (Exception e) {
+                // System.out.println("Error executing PerRequestScans." + scanCheck.getName() +
+                // ": ");
+                e.printStackTrace();
+            }
+        }
+        return issues;
     }
 
     @Override
     public int consolidateDuplicateIssues(IScanIssue existingIssue, IScanIssue newIssue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'consolidateDuplicateIssues'");
+        return isSameIssue(existingIssue, newIssue);
     }
 
     private boolean shouldTriggerPerRequestAttacks(IHttpRequestResponse baseRequestResponse,
@@ -40,18 +51,40 @@ public class PerRequestScans implements IScannerCheck {
         IRequestInfo request = helpers.analyzeRequest(baseRequestResponse.getRequest());
         List<IParameter> params = request.getParameters();
         if (params.size() > 0) {
-            int first_parameter_offset = 999999;
-            IParameter first_parameter = null;
-            for (IParameter param : params) {
-                if (param.getType() == IParameter.PARAM_URL) {
-                    if (param.getName().length() < first_parameter_offset) {
-                        first_parameter_offset = param.getName().length();
-                        first_parameter = param;
+            int firstParameterOffset = 999999;
+            IParameter firstParameter = null;
+            for (byte paramType : new byte[] { IParameter.PARAM_BODY, IParameter.PARAM_URL, IParameter.PARAM_JSON,
+                    IParameter.PARAM_XML, IParameter.PARAM_XML_ATTR, IParameter.PARAM_MULTIPART_ATTR,
+                    IParameter.PARAM_COOKIE }) {
+                for (IParameter param : params) {
+                    if (param.getType() != paramType) {
+                        continue;
+                    }
+                    if (param.getNameStart() < firstParameterOffset) {
+                        firstParameterOffset = param.getNameStart();
+                        firstParameter = param;
                     }
                 }
+                if (firstParameter != null) {
+                    break;
+                }
             }
+
+            if (firstParameter != null && firstParameter.getName() == insertionPoint.getInsertionPointName()) {
+                return true;
+            }
+        } else if (insertionPoint.getInsertionPointType() == IScannerInsertionPoint.INS_HEADER
+                && insertionPoint.getInsertionPointName() == "User-Agent") {
+            return true;
         }
         return false;
+    }
+
+    private int isSameIssue(IScanIssue existingIssue, IScanIssue newIssue) {
+        if (existingIssue.getIssueName() == newIssue.getIssueName()) {
+            return -1;
+        }
+        return 0;
     }
 
 }
