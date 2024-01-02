5ffb60b8279b545a4651023341c3a29ccca32658
Dat Nguyen <dat.nguyen@bitbytelab.io>
2024-01-02 11:15:00 +0700

    Update build.gradle and ActiveScanPlusPlus.java

--- src/io/bitbytelab/activescanpp/ActiveScanPlusPlus.java
+++ src/io/bitbytelab/activescanpp/ActiveScanPlusPlus.java
@@ -1,1 +1,52 @@
 package io.bitbytelab.activescanpp;
+
+import burp.api.montoya.scanner.AuditResult;
+import burp.api.montoya.scanner.ConsolidationAction;
+import burp.api.montoya.scanner.ScanCheck;
+import burp.api.montoya.scanner.audit.insertionpoint.AuditInsertionPoint;
+import burp.api.montoya.scanner.audit.issues.AuditIssue;
+import burp.api.montoya.BurpExtension;
+import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.http.message.HttpRequestResponse;
+import java.nio.charset.StandardCharsets;
+
+public class ActiveScanPlusPlus implements ScanCheck, BurpExtension {
+
+    @Override
+    public void initialize(MontoyaApi api) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+
+    @Override
+    public AuditResult activeAudit(HttpRequestResponse baseRequestResponse, AuditInsertionPoint auditInsertionPoint) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'activeAudit'");
+    }
+
+    @Override
+    public AuditResult passiveAudit(HttpRequestResponse baseRequestResponse) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'passiveAudit'");
+    }
+
+    @Override
+    public ConsolidationAction consolidateIssues(AuditIssue newIssue, AuditIssue existingIssue) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'consolidateIssues'");
+    }
+
+    static String safeBytesToString(byte[] bytes) {
+        if (bytes == null) {
+            bytes = new byte[0];
+        }
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+    static String htmlEncode(String string) {
+        if (string == null) {
+            return null;
+        }
+        return string.replace("<", "&lt;").replace(">", "&gt;");
+    }
+}
---
+++ src/io/bitbytelab/activescanpp/PerHostScans.java
@@ -0,0 +1,61 @@
+package io.bitbytelab.activescanpp;
+
+import java.util.List;
+
+import burp.api.montoya.http.message.HttpRequestResponse;
+import burp.api.montoya.scanner.AuditResult;
+import burp.api.montoya.scanner.ConsolidationAction;
+import burp.api.montoya.scanner.ScanCheck;
+import burp.api.montoya.scanner.audit.insertionpoint.AuditInsertionPoint;
+import burp.api.montoya.scanner.audit.issues.AuditIssue;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PerHostScans implements ScanCheck {
+    List<String> scanned_hosts;
+
+    @Override
+    public AuditResult activeAudit(HttpRequestResponse baseRequestResponse, AuditInsertionPoint auditInsertionPoint) {
+        String host = baseRequestResponse.request().url();
+        if (scanned_hosts.contains(host)) {
+            return null;
+        }
+        scanned_hosts.add(host);
+        List<AuditIssue> issues = null;
+
+        throw new UnsupportedOperationException("Unimplemented method 'activeAudit'");
+    }
+
+    @Override
+    public AuditResult passiveAudit(HttpRequestResponse baseRequestResponse) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ConsolidationAction consolidateIssues(AuditIssue newIssue, AuditIssue existingIssue) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'consolidateIssues'");
+    }
+
+    static List<AuditIssue> interestingFileScan(HttpRequestResponse baseRequestResponse) {
+        List<AuditIssue> issues = null;
+
+        for (Map.Entry<String, String> interestingFileMapping : interestingFileMappings().entrySet()) {
+            String interestingFile = interestingFileMapping.getKey();
+            String interestingFileContent = interestingFileMapping.getValue();
+
+        }
+
+        return issues;
+    }
+
+    static Map<String, String> interestingFileMappings() {
+        Map<String, String> interestingFileMappings = new HashMap<>();
+        interestingFileMappings.put("/.git/config", "[core]");
+        interestingFileMappings.put("/server-status", "Server uptime");
+        interestingFileMappings.put("/.well-known/apple-app-site-association", "applinks");
+        return interestingFileMappings;
+    }
+
+}
